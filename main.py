e2df03e459c030afbed59736215b89ff52b93158
Michael <michaeltheisen@hotmail.com>
2023-04-03 19:40:46 -0700

    changed background from blue to green

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tkinter import *
 window = Tk() # instantiate an instance of a window 
 window.geometry("700x500")
 window.title("Seating Chart GUI")
-window.config(background="#89ccf0")
+window.config(background="#2f5235")
 
 
 window.mainloop() # place window on the computer screen.
