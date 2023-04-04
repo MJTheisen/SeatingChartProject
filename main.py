6fe47a607161d4be31d3c4410a3179c4204c02df
Michael <michaeltheisen@hotmail.com>
2023-04-03 20:18:17 -0700

    blue

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from tkinter import *
 
 window = Tk() # instantiate an instance of a window 
-window.geometry("700x500")
+window.geometry("1500x900")
 window.title("Seating Chart GUI")
-window.config(background="#2f5235")
+window.config(background="#77b2e6")
+
+
 
 
 window.mainloop() # place window on the computer screen.
