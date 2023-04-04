c95f41d82156802f35f4176323987634c694f3d5
Michael <michaeltheisen@hotmail.com>
2023-04-03 19:35:22 -0700

    added blue background

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ from tkinter import *
 window = Tk() # instantiate an instance of a window 
 window.geometry("700x500")
 window.title("Seating Chart GUI")
+window.config(background="#89ccf0")
+
 
 window.mainloop() # place window on the computer screen.
